a0827f04ab491dc5f90ae299f6f858c626edd289
yh <408309839@qq.com>
2024-11-27 10:32:31 +0800

    fix

---
+++ Toos/PbToTs/to_ts/PbToGo.go
@@ -0,0 +1,12 @@
+package to_ts
+
+type PbToGo struct {
+}
+
+func (t *PbToGo) OpenExcelFile(name string, path string, output string) {
+
+}
+
+func NewPbToGo() *PbToGo {
+	return &PbToGo{}
+}
---
+++ Toos/PbToTs/to_ts/PbToTsMain.go
@@ -0,0 +1,16 @@
+package to_ts
+
+type PbToTsMain struct {
+}
+
+func (m PbToTsMain) CheckToTime() bool {
+	return false
+}
+
+func (m PbToTsMain) Hang() {
+
+}
+
+func (m PbToTsMain) DeleteFiles(output string, s string) {
+
+}
