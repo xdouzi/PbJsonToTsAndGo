5281d238c25da27aef9958548454b5efe5bc1959
yh <408309839@qq.com>
2024-11-28 11:12:24 +0800

    fix

--- Toos/pb-gen-JsonToos/pb-gen-JsonToos.go
+++ Toos/pb-gen-JsonToos/pb-gen-JsonToos.go
@@ -10,9 +10,28 @@ import (
 func main() {
 	// 创建应用
 	myApp := app.New()
-	myWindow := myApp.NewWindow("Button Example")
+	myWindow := myApp.NewWindow("pb-gen-JsonToos ")
 	// 设置窗口大小为 800x600
 	myWindow.Resize(fyne.NewSize(800, 600))
+	//---------------------------------------------------------------------------
+
+	// 创建一个文本控件
+	txt_1 := widget.NewLabel("通过Protol里的 *.proto 生成对应的Protocal") // 设置文本内容
+	// 设置按钮的位置和大小
+	txt_1.Resize(fyne.NewSize(200, 40)) // 宽高为 100x40
+	txt_1.Move(fyne.NewPos(300, 10))    // 坐标 x=10, y=10
+
+	// 创建一个文本控件
+	txt_2 := widget.NewLabel("author:yh :技术支持联系408309839@qq.com") // 设置文本内容
+	// 设置按钮的位置和大小
+	txt_2.Resize(fyne.NewSize(200, 40)) // 宽高为 100x40
+	txt_2.Move(fyne.NewPos(300, 30))    // 坐标 x=10, y=10
+
+	// 创建一个文本控件
+	txt_3 := widget.NewLabel("针对cocos 项目功能和web网站json传输,强转对应语言协议结构类型") // 设置文本内容
+	// 设置按钮的位置和大小
+	txt_3.Resize(fyne.NewSize(200, 40)) // 宽高为 100x40
+	txt_3.Move(fyne.NewPos(300, 50))    // 坐标 x=10, y=10
 	//---------------------------------------------------------------------------
 	// 创建按钮
 	but_ts := widget.NewButton("json-gen-ts", func() {
@@ -40,6 +59,7 @@ func main() {
 
 	// 使用自由布局
 	myWindow.SetContent(container.NewWithoutLayout(
+		txt_1, txt_2, txt_3,
 		but_ts,
 		but_go,
 		but_java,
