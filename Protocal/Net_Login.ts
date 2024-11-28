5ad9be9bc90ecca132a22deb357bdda175e89752
yh <408309839@qq.com>
2024-11-28 18:13:48 +0800

    fix

--- Protocal/Net_Login.ts
+++ Protocal/Net_Login.ts
@@ -9,24 +9,13 @@ export namespace pb {
 
  export class Net_Login_LoginReq{
   public DeviceId:string;
-  public FacebookId:string;
-  public AppleId:string;
-  public HmsId:string;
-  public Credential:string;
-  public Package:string;
-  public DeviceModel:string;
-  public SysVersion:string;
+  public LoginListInfo:Base_Login_LoginInfo[];
+  public LoginMapInfo: Map<number, Base_Login_LoginInfo>;
   public LoginInfo:Base_Login_LoginInfo;
  }
 
  export class Net_Login_LoginRet{
   public Ret:number;
-  public Uid:number;
-  public AntiWallow:number;
-  public Credential:string;
-  public ServerTime:number;
-  public DifferentDevice:number;
-  public IsFirstLogin:boolean;
  }
 
 }
--- Toos/json-gen-ts/to_ts/ClassTsTableInfo.go
+++ Toos/json-gen-ts/to_ts/ClassTsTableInfo.go
@@ -17,12 +17,43 @@ func NewClassTsTableInfo() *ClassTsTableInfo {
 
 func (t *ClassTsTableInfo) DoData(m *proto.Message) {
 	t.WLine("export class %s{", m.Name)
+
+	// 遍历消息的所有字段
 	for _, e := range m.Elements {
-		if field, ok := e.(*proto.NormalField); ok {
-			fmt.Printf("  Field: %s:%s \n", field.Name, field.Type)
-			t.WLine(" public %s:%s;", field.Name, t.GetOTypeByPbType(field.Type))
+		switch field := e.(type) {
+		case *proto.NormalField:
+			// 普通字段
+			fmt.Printf("Normal Field: %s:%s\n", field.Name, field.Type)
+			if field.Repeated == true {
+				//数组
+				t.WLine(" public %s:%s[];", field.Name, t.GetOTypeByPbType(field.Type))
+			} else {
+				//普通
+				t.WLine(" public %s:%s;", field.Name, t.GetOTypeByPbType(field.Type))
+			}
+
+		case *proto.MapField:
+			t.WLine(" public %s: Map<%s, %s>;", field.Name, t.GetOTypeByPbType(field.KeyType), t.GetOTypeByPbType(field.Type))
+		case *proto.Comment: //RepeatedField
+			//注视了的不处理
+			//t.WLine(" public %s:%s;", field.Message(), t.GetOTypeByPbType(field.Message()))
+		// Repeated 类型字段
+		//fmt.Printf("Repeated Field: %s (Type: %s)\n", field.Name, field.Type)
+		default:
+			// 其他类型
+			fmt.Printf("Other Field Type: %T\n", field)
 		}
 	}
+	/*
+		for _, e := range m.Elements {
+			log.Printf("---xxxxx--%s", e)
+			if field, ok := e.(*proto.NormalField); ok {
+				fmt.Printf("  Field: %s:%s \n", field.Name, field.Type)
+				t.WLine(" public %s:%s;", field.Name, t.GetOTypeByPbType(field.Type))
+			}
+		}
+	*/
+
 	t.WLine("}")
 }
 
